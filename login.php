9c0a6aceee6e4bfdd59e56fdc6ab01e78ae6b8f4
georgerhughes <42551512+georgerhughes@users.noreply.github.com>
2019-12-03 14:43:10 -0700

    commit changes

---
+++ login.php
@@ -0,0 +1,36 @@
+<!--  George Hughes -->
+<!-- Alli Gilbreath -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Activity Agenda</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="header">
+<ul>
+<li><a href="./home.php?mode=home">Home</a></li>
+<li><a href="./login.php?mode=login">Login</a></li>
+<li><a href="./register.php?mode=register">Sign Up</a></li>
+</ul>
+</div>
+<hr>
+<div id="body">
+<h1>Login</h1>
+<div id="add">
+<form><fieldset>
+<legend>Account Details</legend>
+<div>
+<p>
+<label>Username </label><input type="text" id="username"><br><br>
+<label>Password </label><input type="text" id="password"><br><br>
+<input type="submit" id="submit">
+</p>
+</div>
+</fieldset></form>
+</div>
+</div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,39 @@
+<!--  George Hughes -->
+<!-- Alli Gilbreath -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Activity Agenda</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="header">
+<ul>
+<li><a href="./home.php?mode=home">Home</a></li>
+<li><a href="./login.php?mode=login">Login</a></li>
+<li><a href="./register.php?mode=register">Sign Up</a></li>
+</ul>
+</div>
+<hr>
+<div id="body">
+<h1>Sign Up</h1>
+<div id="add">
+<form><fieldset>
+<legend>Create Account</legend>
+<div>
+<p>
+<label>Username </label><input type="text" id="username"><br><br>
+<label>Password </label><input type="text" id="password"><br><br>
+<label>Re-Enter Password </label><input type="text" id="passwordtwo"><br><br>
+<label>Height </label><input type="text" id="height"><br><br>
+<label>Weight </label><input type="text" id="weight"><br><br>
+<input type="submit" id="submit">
+</p>
+</div>
+</fieldset></form>
+</div>
+</div>
+</body>
+</html>
